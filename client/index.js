f221d99e70faa83f3780cbb38b18cdc15301e1d2
Don Smith <don@devacademy.co.nz>
2020-02-19 08:43:41 +1300

    fix createStore with optional dev tools

--- client/index.js
+++ client/index.js
@@ -8,9 +8,9 @@ import thunk from 'redux-thunk'
 import reducers from './reducers'
 import App from './components/App'
 
-const store = createStore(reducers, compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+  applyMiddleware(thunk)
 ))
 
 document.addEventListener('DOMContentLoaded', () => {
